522ed727e85d4fd77f1946cc834573ef9b8728fa
ptahmose <jbohl@h-quer.de>
2022-11-06 19:47:55 +0100

    cosmetic

--- wxWidgetsTest/utilities.cpp
+++ wxWidgetsTest/utilities.cpp
@@ -1,12 +1,7 @@
 #include "utilities.h"
-#include <cwctype>
 
 std::wstring convertUtf8ToWide(const std::string& str)
 {
-    //std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-    //std::wstring conv = converter.from_bytes(str);
-    //return conv;
-
     std::wstring conv(str.size(), 0);
     size_t size = std::mbstowcs(&conv[0], str.c_str(), str.size());
     conv.resize(size);
@@ -15,12 +10,8 @@ std::wstring convertUtf8ToWide(const std::string& str)
 
 std::string convertWideToUtf8(const std::wstring& str)
 {
-    //std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
-    //std::string conv = converter.to_bytes(str);
-    //return conv;
     size_t requiredSize = std::wcstombs(nullptr, str.c_str(), 0);
     std::string conv(requiredSize, 0);
     conv.resize(std::wcstombs(&conv[0], str.c_str(), requiredSize));
     return conv;
-
 }
